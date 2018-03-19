22f6be9d1f80c733292dc22d992d85211b41eefa
jghoover <30665181+jghoover@users.noreply.github.com>
2018-03-19 10:01:01 -0500

    Update token.py

--- examples/token.py
+++ examples/token.py
@@ -1,1 +1,49 @@
+import enum
 
+
+class Tokens(object):
+    def __init__(self):
+        self._data = []
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def append(self, token):
+        self._data.append(token)
+
+    @property
+    def lookahead(self):
+        return self._data[0]
+
+    def consume(self):
+        self._data.pop(0)
+
+
+class Token(object):
+    def __init__(self, kind, value):
+        self._kind = kind
+        self._value = value
+
+    def __str__(self):
+        return "{0} {1}".format(self.kind, self.value)
+
+    @property
+    def kind(self):
+        return self._kind
+
+    @property
+    def value(self):
+        return self._value
+
+
+# different kinds of tokens
+class TokenEnum(enum.Enum):
+
+    def __str__(self):
+        return self.name
+
+    VARIABLE = enum.auto()
+    OP = enum.auto()
+    OPEN_PAREN = enum.auto()
+    CLOSE_PAREN = enum.auto()
+    EOF = enum.auto()
